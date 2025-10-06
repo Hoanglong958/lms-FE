0c9706b2ba39d012ddcc63c4fef047a1cb6adbcc
Đào Nguyên Anh <daonguyenanhprivate@gmail.com>
2025-10-06 22:21:17 +0700

    fix login

--- src/features/login/pages/Login.jsx
+++ src/features/login/pages/Login.jsx
@@ -1,6 +1,5 @@
 import { useState } from "react";
 import { useNavigate, Link } from "react-router-dom";
-import "./login.css";
 
 export default function Login() {
   const [email, setEmail] = useState("");
