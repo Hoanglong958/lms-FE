6590e4e2a70cd0e3f0ec87132e188573a3e265e9
Đào Nguyên Anh <daonguyenanhprivate@gmail.com>
2025-10-08 21:56:01 +0700

    fix:footer

--- src/layouts/LayoutFooter.jsx
+++ src/layouts/LayoutFooter.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Footer.css";
 import mankaiLogo from "@assets/logos/mankai-logo.svg";
 import facebookIcon from "@assets/icons/facebook-icon.svg";
